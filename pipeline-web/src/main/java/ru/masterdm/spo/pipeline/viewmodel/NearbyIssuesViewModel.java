04e1ecd91fc7f75b939ff1adf81e7cd7462f55a4
ildar shafigullin <ildar66@inbox.ru>
2017-10-13 13:41:09 +0300

    add template for hedders column in nearby Issues page.

--- pipeline-web/src/main/java/ru/masterdm/spo/pipeline/viewmodel/NearbyIssuesViewModel.java
+++ pipeline-web/src/main/java/ru/masterdm/spo/pipeline/viewmodel/NearbyIssuesViewModel.java
@@ -23,9 +23,9 @@ public class NearbyIssuesViewModel {
     @Init
     public void init() {
         _columns = new ArrayList<ColumnInfo>();
-        _columns.add(new ColumnInfo("name", "Name", true, "label"));
-        _columns.add(new ColumnInfo("date", "Birth", true, "date"));
-        _columns.add(new ColumnInfo("sum", "Marital status", false, "sum"));
+        _columns.add(new ColumnInfo("name", " ", true, "label"));
+        _columns.add(new ColumnInfo("date", "Дата выдачи.", true, "date"));
+        _columns.add(new ColumnInfo("sum", "Сумма в вал. сделки.", true, "sum"));
 
         _data = provideData();
     }
@@ -35,7 +35,7 @@ public class NearbyIssuesViewModel {
 
         data.add(new DealIssue("ГК ПИК", 1980, 1, 20, 12000));
         data.add(new DealIssue("ГК 'Химнефть'", 1982, 2, 11, 15000));
-        data.add(new DealIssue("ГК Строитель", 1979, 3, 21, 7000));
+        data.add(new DealIssue("ГК Строитель", 2017, 3, 21, 7000));
 
         return data;
     }
@@ -46,5 +46,37 @@ public class NearbyIssuesViewModel {
      */
     public ArrayList<DealIssue> getIssues() {
         return _data;
+    }
+
+    public ArrayList<ColumnInfo> getVisibleColumns() {
+        return getColumns(Filter.VISIBLE);
+    }
+
+    private ArrayList<ColumnInfo> getColumns(Filter filter) {
+        ArrayList<ColumnInfo> list = new ArrayList<ColumnInfo>();
+        for (ColumnInfo info : _columns) {
+            if (filter.accept(info))
+                list.add(info);
+        }
+        return list;
+    }
+
+    interface Filter {
+
+        public boolean accept(ColumnInfo info);
+
+        Filter VISIBLE = new Filter() {
+
+            public boolean accept(ColumnInfo info) {
+                return info.isVisible();
+            }
+        };
+
+        Filter HIDDEN = new Filter() {
+
+            public boolean accept(ColumnInfo info) {
+                return !info.isVisible();
+            }
+        };
     }
 }
